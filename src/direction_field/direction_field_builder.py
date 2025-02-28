fix bug with scaling when changing between equal and auto axis

src/direction_field/direction_field_builder.py
@@ -13,12 +13,12 @@ from src.direction_field.direction_field_settings import DirectionFieldSettings
 class DirectionFieldBuilder:
     """Class for calculating arrows and colors for drawing direction fields."""
 
-    def __init__(self, plot_axes: Axes, settings: DirectionFieldSettings):
+    def __init__(self, plot_axes: Axes, settings: DirectionFieldSettings) -> None:
         self.__settings = settings
         self.__arrows_cache: Dict[Tuple[float, float], NDArray[np.float64]] = {}
         self.__plot_axes = plot_axes
 
-    def clear_arrow_cache(self):
+    def clear_arrow_cache(self) -> None:
         """Clears the arrow cache."""
         self.__arrows_cache = {}
 
@@ -72,7 +72,7 @@ class DirectionFieldBuilder:
 
         return result
 
-    def get_curvature_at(self, x: float, y: float, dx: float) -> float:
+    def __get_curvature_at(self, x: float, y: float, dx: float) -> float:
         """
         Returns the curvature of the function at the point (x, y).
 
@@ -118,7 +118,7 @@ class DirectionFieldBuilder:
                     # if both fail, return 0
                     return 0
 
-    def normalize_curvatures(
+    def __normalize_curvatures(
         self, curvatures: NDArray[np.float64], off_screen: Sequence[bool]
     ) -> NDArray[np.float64]:
         """Normalizes curvatures to values to `[0,1]`. When the max curvature for normalization is searched for,
@@ -184,13 +184,13 @@ class DirectionFieldBuilder:
         curvatures: list[float] = []
         off_screen: list[bool] = []
         for x, y in points:
-            curvatures.append(self.get_curvature_at(x, y, curvature_dx))
+            curvatures.append(self.__get_curvature_at(x, y, curvature_dx))
             off_screen.append(
                 False if (xlim[0] <= x <= xlim[1] and ylim[0] <= y <= ylim[1]) else True
             )
 
         # normalize and scale curvatures before mapping them to colors
-        normalized_curvatures = self.normalize_curvatures(np.abs(curvatures), off_screen)
+        normalized_curvatures = self.__normalize_curvatures(np.abs(curvatures), off_screen)
         exponent = self.__settings.color_exp
         color_map = self.__settings.color_map
         return cm.get_cmap(color_map)(normalized_curvatures**exponent)

src/gui/visualizer_app.py
@@ -25,14 +25,13 @@ from src.gui.stop_button import StopButton
 from src.gui.component_builder import QtComponentBuilder
 from src.canvas import Canvas
 from src.default_constants import *
+from src.math_functions import try_get_value_from_string
 
 from typing import override
 
 
 class VisualizerApp(QWidget):
     """Creates the GUI using the PyQt6 library."""
-
-    equal_axes = True  # True if the 'Equal axes' checkbox is checked
 
     def __init__(self) -> None:
         super().__init__()
@@ -215,7 +214,7 @@ class VisualizerApp(QWidget):
             layout=layout,
         )
 
-        self.__equalAxes.setChecked(VisualizerApp.equal_axes)
+        self.__equalAxes.setChecked(True)
 
         # create the 'center x' button
         self.__center_x_button = QPushButton("Center &X")
@@ -470,25 +469,22 @@ Shortcut: 'Ctrl+T'"""
             self.__canvas.figure.savefig(file_name, bbox_inches="tight")
 
     def __execute_graph_function(self) -> None:
-        """Plots the function given in the function input line."""
+        """Plots the function given in the function input line and updates the displayed axes limits."""
 
         func_str = self.__function_input.text()
         if not self.__canvas.set_new_function(func_str):
             QMessageBox.critical(self, "Error", f"Invalid function.")
 
+        self.update_displayed_axes_limits()
+
     def __checked_equalAxes(self, checked: bool) -> None:
         """Turns equal_axes on and off."""
-        VisualizerApp.equal_axes = not VisualizerApp.equal_axes
         if checked:
             self.__canvas.set_equal_axes()
             self.__enable_input_lines(False)
         else:
             self.__canvas.set_auto_axes()
             self.__enable_input_lines(True)
-            self.__update_xmin()
-            self.__update_xmax()
-            self.__update_ymin()
-            self.__update_ymax()
 
     def __enable_input_lines(self, enabled: bool) -> None:
         """Enables or disables all of the input lines for x and y limits."""
@@ -570,10 +566,8 @@ Shortcut: 'Ctrl+T'"""
 
     def __update_xmin(self) -> None:
         """Updates xmin according to the xmin input line."""
-        xmin = self.__xmin_input.text()
-        try:
-            xmin = float(xmin)
-        except ValueError:  # don't change anything if the input is not valid
+        xmin = try_get_value_from_string(self.__xmin_input.text())
+        if xmin is None:
             return
         xlim = self.__canvas.xlim
         if xmin == round(xlim[0], ROUND_INPUT_LINES) or xmin >= xlim[1]:
@@ -582,10 +576,8 @@ Shortcut: 'Ctrl+T'"""
 
     def __update_xmax(self) -> None:
         """Updates xmax according to the xmax input line."""
-        xmax = self.__xmax_input.text()
-        try:
-            xmax = float(xmax)
-        except ValueError:  # don't change anything if the input is not valid
+        xmax = try_get_value_from_string(self.__xmax_input.text())
+        if xmax is None:
             return
         xlim = self.__canvas.xlim
         if xmax == round(xlim[1], ROUND_INPUT_LINES) or xmax <= xlim[0]:
@@ -594,10 +586,8 @@ Shortcut: 'Ctrl+T'"""
 
     def __update_ymin(self) -> None:
         """Updates ymin according to the ymin input line."""
-        ymin = self.__ymin_input.text()
-        try:
-            ymin = float(ymin)
-        except ValueError:  # don't change anything if the input is not valid
+        ymin = try_get_value_from_string(self.__ymin_input.text())
+        if ymin is None:
             return
         ylim = self.__canvas.ylim
         if ymin == round(ylim[0], ROUND_INPUT_LINES) or ymin >= ylim[1]:
@@ -606,10 +596,8 @@ Shortcut: 'Ctrl+T'"""
 
     def __update_ymax(self) -> None:
         """Updates ymax according to the ymax input line."""
-        ymax = self.__ymax_input.text()
-        try:
-            ymax = float(ymax)
-        except ValueError:  # don't change anything if the input is not valid
+        ymax = try_get_value_from_string(self.__ymax_input.text())
+        if ymax is None:
             return
         ylim = self.__canvas.ylim
         if ymax == round(ylim[1], ROUND_INPUT_LINES) or ymax <= ylim[0]:
@@ -626,18 +614,19 @@ Shortcut: 'Ctrl+T'"""
 
     def __update_num_arrows(self) -> None:
         """Updates the number of arrows according to the input line."""
-        num_arrows = self.__num_arrows_input.text()
-        try:
-            num_arrows = int(num_arrows)
-        except ValueError:  # don't change anything if the input is not valid
+        num_arrows = try_get_value_from_string(self.__num_arrows_input.text())
+        if num_arrows is None:
             return
+
         if num_arrows < MIN_NUM_ARROWS or num_arrows > MAX_NUM_ARROWS:
             num_arrows = np.clip(num_arrows, MIN_NUM_ARROWS, MAX_NUM_ARROWS)
             self.__num_arrows_input.setText(str(num_arrows))
         if num_arrows < 1:
             num_arrows = 1
             self.__num_arrows_input.setText(str(num_arrows))
-        self.__canvas.set_num_arrows(num_arrows)
+
+        self.__num_arrows_input.setText(str(int(num_arrows)))
+        self.__canvas.set_num_arrows(int(num_arrows))
 
     def __add_five_arrows(self) -> None:
         """Adds 5 arrows."""

src/tracing/solution_tracer.py
@@ -8,20 +8,14 @@ from src.default_constants import TRACE_NUM_SEGMENTS_IN_DIAGONAL
 from src.tracing.numerical_methods import find_first_intersection
 from src.tracing.trace_settings import TraceSettings
 
-from typing import Tuple, Iterator, TypeAlias, Any
-
-
-Direction: TypeAlias = int
-"""Type alias for tracing direction."""
-
-Strategy: TypeAlias = int
-"""Type alias for singularity handling strategy"""
+from typing import Tuple, Iterator, Any
+from enum import Enum
 
 
 class SolutionTracer:
     """Class for tracing a solution curve with an initial point and a given slope function."""
 
-    class Direction:
+    class Direction(Enum):
         """Tracing direction constants."""
 
         Right = 1
@@ -29,7 +23,7 @@ class SolutionTracer:
         Up = 1
         Down = -1
 
-    class Strategy:
+    class Strategy(Enum):
         """Singularity handling return codes."""
 
         Stop = 1
@@ -90,7 +84,7 @@ class SolutionTracer:
         self.__max_line_segment_length = self.__diagonal_len / TRACE_NUM_SEGMENTS_IN_DIAGONAL
 
         # private fields for tracing
-        self.__direction: Direction
+        self.__direction: SolutionTracer.Direction
         """Tracing direction. Either SolutionTracer.Direction.Right or SolutionTracer.Direction.Left."""
         self.__slope: float
         """Current slope of the solution curve."""
@@ -425,7 +419,7 @@ class SolutionTracer:
 
         # starting point
         point = np.array([x0, y0])
-        current_line_segment_length = 0
+        current_line_segment_length: float = 0
         line_segment_start = point.copy()
 
         def get_y_step(y: float) -> float:
@@ -437,7 +431,7 @@ class SolutionTracer:
             else:
                 dist = fabs(y - self.__ylim[0]) if y < self.__ylim[0] else fabs(y - self.__ylim[1])
                 step = max(dist / 100, self.__max_step)
-            return step * direction
+            return step * direction.value
 
         while True:
             # if y out of bounds in the desired direction --> break
@@ -596,17 +590,17 @@ class SolutionTracer:
 
         # the number of times in a row the tracing continued OK after a singularity was detected
         # it is used in automatic detection - if it continues OK for a while, it is probably safe
-        continue_count = 0
+        continue_count: int = 0
 
         # length of the current curve segment
-        current_curve_segment_length = 0
+        current_curve_segment_length: float = 0
         line_segment_start = point.copy()
 
         while True:
             try:
                 # calculate the slope at the current point
                 self.__slope = self.__slope_func(point[0], point[1])
-                self.__vector = np.array([1, self.__slope], dtype=np.float64) * direction
+                self.__vector = np.array([1, self.__slope], dtype=np.float64) * direction.value
             except:
                 # slope_func is unsafe
                 break
@@ -642,7 +636,7 @@ class SolutionTracer:
                         yield (point[0], point[1])
                         return
 
-                    line_direction = sign(self.__slope) * direction
+                    line_direction = SolutionTracer.Direction(sign(self.__slope) * direction.value)
 
                     yield from self.__create_vertical_line(point[0], point[1], line_direction)
                     return