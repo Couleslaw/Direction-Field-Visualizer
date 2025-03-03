Couleslaw/Direction-Field-Visualizer

commit 020db7d680c54722a0d50538f8041ae3f23d3b7b

fix bug with scaling when changing between equal and auto axis

src/threading/parallel_tracer.py
@@ -1,7 +1,7 @@
 from PyQt6.QtCore import pyqtSignal, QObject, QMutex
 from typing import List, Tuple
 
-from src.tracing.solution_tracer import SolutionTracer, Direction
+from src.tracing.solution_tracer import SolutionTracer
 from src.tracing.trace_settings import TraceSettings, CurveInfo
 
 
@@ -16,7 +16,7 @@ class ParallelTracer(QObject):
         y: float,
         xlim: Tuple[float, float],
         ylim: Tuple[float, float],
-        direction: Direction,
+        direction: SolutionTracer.Direction,
         slope_function_str: str,
         trace_settings: TraceSettings,
     ) -> None:

src/tracing/solution_tracer.py
@@ -8,20 +8,14 @@ from src.default_constants import TRACE_NUM_SEGMENTS_IN_DIAGONAL
 from src.tracing.numerical_methods import find_first_intersection
 from src.tracing.trace_settings import TraceSettings
 
-from typing import Tuple, Iterator, TypeAlias, Any
-
-
-Direction: TypeAlias = int
-"""Type alias for tracing direction."""
-
-Strategy: TypeAlias = int
-"""Type alias for singularity handling strategy"""
+from typing import Tuple, Iterator, Any
+from enum import Enum
 
 
 class SolutionTracer:
     """Class for tracing a solution curve with an initial point and a given slope function."""
 
-    class Direction:
+    class Direction(Enum):
         """Tracing direction constants."""
 
         Right = 1
@@ -29,7 +23,7 @@ class SolutionTracer:
         Up = 1
         Down = -1
 
-    class Strategy:
+    class Strategy(Enum):
         """Singularity handling return codes."""
 
         Stop = 1
@@ -90,7 +84,7 @@ class SolutionTracer:
         self.__max_line_segment_length = self.__diagonal_len / TRACE_NUM_SEGMENTS_IN_DIAGONAL
 
         # private fields for tracing
-        self.__direction: Direction
+        self.__direction: SolutionTracer.Direction
         """Tracing direction. Either SolutionTracer.Direction.Right or SolutionTracer.Direction.Left."""
         self.__slope: float
         """Current slope of the solution curve."""
@@ -425,7 +419,7 @@ class SolutionTracer:
 
         # starting point
         point = np.array([x0, y0])
-        current_line_segment_length = 0
+        current_line_segment_length: float = 0
         line_segment_start = point.copy()
 
         def get_y_step(y: float) -> float:
@@ -437,7 +431,7 @@ class SolutionTracer:
             else:
                 dist = fabs(y - self.__ylim[0]) if y < self.__ylim[0] else fabs(y - self.__ylim[1])
                 step = max(dist / 100, self.__max_step)
-            return step * direction
+            return step * direction.value
 
         while True:
             # if y out of bounds in the desired direction --> break
@@ -596,17 +590,17 @@ class SolutionTracer:
 
         # the number of times in a row the tracing continued OK after a singularity was detected
         # it is used in automatic detection - if it continues OK for a while, it is probably safe
-        continue_count = 0
+        continue_count: int = 0
 
         # length of the current curve segment
-        current_curve_segment_length = 0
+        current_curve_segment_length: float = 0
         line_segment_start = point.copy()
 
         while True:
             try:
                 # calculate the slope at the current point
                 self.__slope = self.__slope_func(point[0], point[1])
-                self.__vector = np.array([1, self.__slope], dtype=np.float64) * direction
+                self.__vector = np.array([1, self.__slope], dtype=np.float64) * direction.value
             except:
                 # slope_func is unsafe
                 break
@@ -642,7 +636,7 @@ class SolutionTracer:
                         yield (point[0], point[1])
                         return
 
-                    line_direction = sign(self.__slope) * direction
+                    line_direction = SolutionTracer.Direction(sign(self.__slope) * direction.value)
 
                     yield from self.__create_vertical_line(point[0], point[1], line_direction)
                     return

src/canvas.py
@@ -1,6 +1,7 @@
 # importing VisualizerApp for type annotations like this to prevent circular imports
 from __future__ import annotations
 from typing import Tuple, TYPE_CHECKING
+from math import sqrt
 
 if TYPE_CHECKING:
     from src.gui.visualizer_app import VisualizerApp
@@ -196,8 +197,28 @@ class Canvas(FigureCanvas):
 
     def set_equal_axes(self) -> None:
         """Sets the plot axes to have equal scaling. Redraws the plot after."""
-        self.redraw()
+
+        # we will scale the axes so that the diagonal length and center of the plot is the same as before, but the axes are equal
+
+        old_diagonal = sqrt((self.xlim[1] - self.xlim[0]) ** 2 + (self.ylim[1] - self.ylim[0]) ** 2)
+
         self.__axes.axis("equal")
+        self.redraw()
+
+        # r = dx / dy  -->  dx = r * dy
+        ratio = (self.xlim[1] - self.xlim[0]) / (self.ylim[1] - self.ylim[0])
+
+        # diagonal^2 = dx^2 + dy^2 = dy^2 * (r^2 + 1)  --> dy = diagonal / sqrt(r^2 + 1)
+        dy = old_diagonal / sqrt(ratio**2 + 1)
+        dx = dy * ratio
+
+        x_center = (self.xlim[1] + self.xlim[0]) / 2
+        y_center = (self.ylim[1] + self.ylim[0]) / 2
+
+        # set the new axes limits
+        self.xlim = (x_center - dx / 2, x_center + dx / 2)
+        self.ylim = (y_center - dy / 2, y_center + dy / 2)
+
         self.redraw()
         self.app.update_displayed_axes_limits()
 
@@ -206,7 +227,11 @@ class Canvas(FigureCanvas):
         Sets the plot axes to have automatic scaling. Redraws the plot after.
         This is used when the user wants to set custom axes limits.
         """
+        old_xlim = self.xlim
+        old_ylim = self.ylim
         self.__axes.axis("auto")
+        self.xlim = old_xlim
+        self.ylim = old_ylim
         self.redraw()
         self.app.update_displayed_axes_limits()
 

src/canvas_manager.py
@@ -20,10 +20,8 @@ from src.tracing.trace_settings import TraceSettings
 from src.tracing.solution_tracer import SolutionTracer
 
 from src.direction_field.direction_field_settings import DirectionFieldSettings
-from src.direction_field.direction_field_builder import (
-    DirectionFieldPlotter,
-    DirectionFieldBuilder,
-)
+from src.direction_field.direction_field_builder import DirectionFieldPlotter, DirectionFieldBuilder
+
 
 from src.threading.parallel_tracer import ParallelTracer
 from src.threading.trace_manager import TraceManager

src/default_constants.py
@@ -116,8 +116,8 @@ AVAILABLE_COLOR_MAPS = [
 
 DEFAULT_FUNCTION = "x/y"
 AXIS_RATIO = 1.5
-DEFAULT_XMIN = -2.5
-DEFAULT_XMAX = 2.5
+DEFAULT_XMIN = -3.5
+DEFAULT_XMAX = 3.5
 DEFAULT_YMIN = DEFAULT_XMIN / AXIS_RATIO
 DEFAULT_YMAX = DEFAULT_XMAX / AXIS_RATIO
 

src/direction_field/direction_field_builder.py
@@ -13,12 +13,12 @@ from src.direction_field.direction_field_settings import DirectionFieldSettings
 class DirectionFieldBuilder:
     """Class for calculating arrows and colors for drawing direction fields."""
 
-    def __init__(self, plot_axes: Axes, settings: DirectionFieldSettings):
+    def __init__(self, plot_axes: Axes, settings: DirectionFieldSettings) -> None:
         self.__settings = settings
         self.__arrows_cache: Dict[Tuple[float, float], NDArray[np.float64]] = {}
         self.__plot_axes = plot_axes
 
-    def clear_arrow_cache(self):
+    def clear_arrow_cache(self) -> None:
         """Clears the arrow cache."""
         self.__arrows_cache = {}
 
@@ -72,7 +72,7 @@ class DirectionFieldBuilder:
 
         return result
 
-    def get_curvature_at(self, x: float, y: float, dx: float) -> float:
+    def __get_curvature_at(self, x: float, y: float, dx: float) -> float:
         """
         Returns the curvature of the function at the point (x, y).
 
@@ -118,7 +118,7 @@ class DirectionFieldBuilder:
                     # if both fail, return 0
                     return 0
 
-    def normalize_curvatures(
+    def __normalize_curvatures(
         self, curvatures: NDArray[np.float64], off_screen: Sequence[bool]
     ) -> NDArray[np.float64]:
         """Normalizes curvatures to values to `[0,1]`. When the max curvature for normalization is searched for,
@@ -184,13 +184,13 @@ class DirectionFieldBuilder:
         curvatures: list[float] = []
         off_screen: list[bool] = []
         for x, y in points:
-            curvatures.append(self.get_curvature_at(x, y, curvature_dx))
+            curvatures.append(self.__get_curvature_at(x, y, curvature_dx))
             off_screen.append(
                 False if (xlim[0] <= x <= xlim[1] and ylim[0] <= y <= ylim[1]) else True
             )
 
         # normalize and scale curvatures before mapping them to colors
-        normalized_curvatures = self.normalize_curvatures(np.abs(curvatures), off_screen)
+        normalized_curvatures = self.__normalize_curvatures(np.abs(curvatures), off_screen)
         exponent = self.__settings.color_exp
         color_map = self.__settings.color_map
         return cm.get_cmap(color_map)(normalized_curvatures**exponent)

src/direction_field/direction_field_settings.py
@@ -1,19 +1,5 @@
 from src.math_functions import create_function_from_string, sqrt
-from src.default_constants import (
-    DEFAULT_FUNCTION,
-    DEFAULT_ARROW_LENGTH,
-    DEFAULT_ARROW_WIDTH,
-    MIN_ARROW_WIDTH,
-    MAX_ARROW_WIDTH,
-    DEFAULT_NUM_ARROWS,
-    DEFAULT_COLOR_MAP,
-    DEFAULT_COLOR_PRECISION,
-    DEFAULT_COLOR_CONTRAST,
-    MIN_COLOR_CONTRAST,
-    MAX_COLOR_CONTRAST,
-    MIN_COLOR_EXP,
-    MAX_COLOR_EXP,
-)
+from src.default_constants import *
 from typing import Tuple, Callable, Any
 
 
@@ -23,7 +9,7 @@ class DirectionFieldSettings:
     # base of an exponential used to scale the curvatures before converting to colors
     __color_exp_base = 1.4
 
-    def __init__(self):
+    def __init__(self) -> None:
         # arrow settings
         self.__arrow_length: int = DEFAULT_ARROW_LENGTH
         self.__arrow_width: int = DEFAULT_ARROW_WIDTH
@@ -49,9 +35,7 @@ class DirectionFieldSettings:
         """Sets new arrow length based on the displayed number."""
         self.__arrow_length = length
 
-    def calculate_arrow_length(
-        self, xlim: Tuple[float, float], ylim: Tuple[float, float]
-    ) -> float:
+    def calculate_arrow_length(self, xlim: Tuple[float, float], ylim: Tuple[float, float]) -> float:
         """Returns the arrow length given limits of the axes."""
         diagonal = sqrt((xlim[1] - xlim[0]) ** 2 + (ylim[1] - ylim[0]) ** 2)
         return diagonal * self.__arrow_length / 200
@@ -71,8 +55,7 @@ class DirectionFieldSettings:
     def color_exp(self) -> float:
         """Returns the exponent for the color contrast."""
         a = (MAX_COLOR_EXP - MIN_COLOR_EXP) / (
-            self.__color_exp_base**MAX_COLOR_CONTRAST
-            - self.__color_exp_base**MIN_COLOR_CONTRAST
+            self.__color_exp_base**MAX_COLOR_CONTRAST - self.__color_exp_base**MIN_COLOR_CONTRAST
         )
         b = MIN_COLOR_EXP - a * self.__color_exp_base**MIN_COLOR_CONTRAST
         return a * self.__color_exp_base**self.color_contrast + b

src/gui/lock_button.py
@@ -7,15 +7,14 @@ from PyQt6.QtWidgets import QPushButton, QWidget
 from PyQt6.QtGui import QIcon
 from PyQt6.QtCore import Qt
 
-from typing import override, TypeAlias
-
-LockState: TypeAlias = int
+from typing import override
+from enum import Enum
 
 
 class LockButton(QPushButton):
     """A button that represents a two state lock."""
 
-    class LockState:
+    class LockState(Enum):
         """Class to represent the state of the lock button."""
 
         Locked = 0

commit 51c09b4ef112eeea5d0777de01cd459837ff07d0

fix: bug introduced by 020db7d where trace curves would not end properly

src/threading/parallel_tracer.py
@@ -104,10 +104,10 @@ class ParallelTracer(QObject):
 
         # create a new solution tracer
         tracer = SolutionTracer(
-            self.__settings, self.__slope_function_str, self.__xlim, self.__ylim
+            self.__settings, self.__slope_function_str, self.__direction, self.__xlim, self.__ylim
         )
         # get the line segment iterator
-        line_iterator = tracer.trace(self.__x, self.__y, self.__direction)
+        line_iterator = tracer.trace(self.__x, self.__y)
 
         # change state to running
         self.__running = True

src/tracing/solution_tracer.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import numpy as np
 from numpy.typing import NDArray
+from numpy import floating
 
 from src.math_functions import sign, fabs, create_function_from_string
 from src.default_constants import TRACE_NUM_SEGMENTS_IN_DIAGONAL
@@ -35,15 +36,15 @@ class SolutionTracer:
         CONTINUE = 3
 
     @staticmethod
-    def vector_length(vector: NDArray[np.float64]) -> float:
+    def vector_length(vector: NDArray[floating]) -> float:
         return float(np.linalg.norm(vector))
 
     @staticmethod
-    def resize_vector(vector: NDArray[np.float64], length: float) -> NDArray[np.float64]:
+    def resize_vector(vector: NDArray[floating], length: float) -> NDArray[floating]:
         return vector * length / SolutionTracer.vector_length(vector)
 
     @staticmethod
-    def resize_vector_by_x(vector: NDArray[np.float64], new_x: float) -> NDArray[np.float64]:
+    def resize_vector_by_x(vector: NDArray[floating], new_x: float) -> NDArray[floating]:
         return vector * new_x / fabs(vector[0])
 
     @staticmethod
@@ -54,6 +55,7 @@ class SolutionTracer:
         self,
         settings: TraceSettings,
         slope_function_string: str,
+        direction: SolutionTracer.Direction,
         xlim: Tuple[float, float],
         ylim: Tuple[float, float],
     ) -> None:
@@ -68,6 +70,7 @@ class SolutionTracer:
 
         # store the arguments
         self.__settings = settings
+        self.__direction = direction
         self.__xlim = xlim
         self.__ylim = ylim
 
@@ -88,7 +91,6 @@ class SolutionTracer:
         self.__max_line_segment_length = self.__diagonal_len / TRACE_NUM_SEGMENTS_IN_DIAGONAL
 
         # private fields for tracing
-        self.__direction: SolutionTracer.Direction
         """Tracing direction. Either Right or Left."""
         self.__slope: float
         """Current slope of the solution curve."""
@@ -258,7 +260,10 @@ class SolutionTracer:
 
             # auto detection --> use sing_dx to determine size of diff
             if self.__detection_strategy == TraceSettings.Strategy.AUTOMATIC:
-                diff = np.array([self.__sing_dx, self.__sing_dx * der]) * self.__direction
+                diff = (
+                    np.array([self.__sing_dx, self.__sing_dx * der], dtype=np.float64)
+                    * self.__direction.value
+                )
 
             # manual detection --> use distance to singularity to determine size of diff
             elif self.__detection_strategy == TraceSettings.Strategy.MANUAL:
@@ -557,7 +562,7 @@ class SolutionTracer:
             # resize vector to have the same dx as is used in singularity detection --> step of this size should be safe
             self.__vector = self.resize_vector_by_x(self.__vector, self.__sing_dx)
 
-    def trace(self, x0: float, y0: float, direction: Direction) -> Iterator[Tuple[float, float]]:
+    def trace(self, x0: float, y0: float) -> Iterator[Tuple[float, float]]:
         """Traces a solution curve starting at `(x0, y0)` in the given direction until it reaches a singularity or goes off screen.
 
         Args:
@@ -577,7 +582,6 @@ class SolutionTracer:
         last_point = point.copy()
 
         # set tracing direction, max_dx and sing_dx
-        self.__direction = direction
         xdiff = self.__xlim[1] - self.__xlim[0]
         self.__max_dx = (xdiff) / 10**self.__settings.trace_dx_granularity
         self.__sing_dx = min(1e-6, self.__max_dx / 1000)
@@ -601,7 +605,9 @@ class SolutionTracer:
             try:
                 # calculate the slope at the current point
                 self.__slope = self.__slope_func(point[0], point[1])
-                self.__vector = np.array([1, self.__slope], dtype=np.float64) * direction.value
+                self.__vector = (
+                    np.array([1, self.__slope], dtype=np.float64) * self.__direction.value
+                )
             except:
                 # slope_func is unsafe
                 break
@@ -637,7 +643,9 @@ class SolutionTracer:
                         yield (point[0], point[1])
                         return
 
-                    line_direction = SolutionTracer.VDirection(sign(self.__slope) * direction.value)
+                    line_direction = SolutionTracer.VDirection(
+                        sign(self.__slope) * self.__direction.value
+                    )
 
                     yield from self.__create_vertical_line(point[0], point[1], line_direction)
                     return